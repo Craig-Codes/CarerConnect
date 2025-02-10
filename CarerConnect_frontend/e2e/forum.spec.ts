c83d21f05aebcffd1e580368ff038cb3833c7f6d
Craig A <craigadam1987@gmail.com>
2025-02-10 20:22:36 +0000

    Add forum e2e tests and restructure e2e folder

---
+++ CarerConnect_frontend/e2e/forum.spec.ts
@@ -0,0 +1,103 @@
+import { test, expect } from "@playwright/test";
+
+test("Standard user can create a forum post, edit it, and then delete it", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").fill("user1@example.com");
+  await page.getByLabel("Password *").fill("password");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByRole("button", { name: "FORUM", exact: true }).click();
+  await page
+    .getByRole("rowheader", { name: "General Discussion Talk about" })
+    .click();
+  await page
+    .getByRole("rowheader", { name: "How Do You Manage Day-to-Day" })
+    .click();
+  await page.getByRole("button", { name: "Create New Post" }).nth(1).click();
+  await page.getByLabel("Content *").fill("Test post - create");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Test post - create").last()).toBeVisible();
+  await page.getByTestId("ModeEditIcon").last().click();
+  await page.getByLabel("content *").click();
+  await page.getByLabel("content *").fill("Test post - edit");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Test post - edit").first()).toBeVisible();
+  await page.getByTestId("DeleteIcon").locator("path").last().click();
+  await page.getByRole("button", { name: "Yes" }).click();
+  await expect(
+    page.getByText("Successfully deleted post").first()
+  ).toBeVisible();
+});
+
+test("User can create thread, post, and delete thread", async ({ page }) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").fill("user2@example.com");
+  await page.getByLabel("Password *").fill("password");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByRole("button", { name: "FORUM", exact: true }).click();
+  await page
+    .getByRole("rowheader", { name: "Health and Well-being Support" })
+    .click();
+  await page.getByRole("button", { name: "Create New Thread" }).click();
+  await page.getByLabel("Title *").fill("Test Thread");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Test Thread")).toBeVisible();
+  await page.getByRole("button", { name: "Create New Thread" }).click();
+  await page.getByLabel("Title *").fill("Test Thread");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page
+    .getByRole("rowheader", { name: "Test Thread Created: 10" })
+    .click();
+  await page.getByRole("button", { name: "Create New Post" }).click();
+  await page.getByLabel("Content *").fill("Test post - delete");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Back" }).click();
+  await page.getByRole("cell", { name: "1" }).click();
+  await page
+    .getByRole("row", { name: "Test Thread Created: 10" })
+    .locator("path")
+    .click();
+  await page.getByRole("button", { name: "Yes" }).click();
+  await page.getByText("Successfully deleted thread").click();
+  await expect(page.getByText("Test Thread")).not.toBeVisible();
+  await page.getByLabel("close").click();
+});
+
+test("Users can see each others posts, and admin user can delete posts", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").fill("user1@example.com");
+  await page.getByLabel("Password *").fill("password");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByRole("button", { name: "FORUM", exact: true }).click();
+  await page
+    .getByRole("rowheader", { name: "General Discussion Talk about" })
+    .click();
+  await page
+    .getByRole("rowheader", { name: "How Do You Manage Day-to-Day" })
+    .click();
+  await page.getByRole("button", { name: "Create New Post" }).nth(1).click();
+  await page.getByLabel("Content *").fill("Post from standard user");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Post from standard user").last()).toBeVisible();
+  await page.getByLabel("close").click();
+  await page.getByRole("button", { name: "FC" }).click();
+  await page.getByText("Logout").click();
+  await page.getByLabel("Email Address *").fill("user2@example.com");
+  await page.getByLabel("Password *").fill("password");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByLabel("View Forum page").click();
+  await page
+    .getByRole("rowheader", { name: "General Discussion Talk about" })
+    .click();
+  await page
+    .getByRole("rowheader", { name: "How Do You Manage Day-to-Day" })
+    .click();
+  await page
+    .locator("div:nth-child(10) > div:nth-child(2) > .MuiSvgIcon-root > path")
+    .click();
+  await page.getByRole("button", { name: "Yes" }).click();
+  await expect(page.getByText("Successfully deleted post")).toBeVisible();
+});
---
+++ CarerConnect_frontend/e2e/loginRegisterAndAuthentication.spec.ts
@@ -0,0 +1,87 @@
+import { test, expect } from "@playwright/test";
+
+test("User can login and sees the navbar buttons", async ({ page }) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").fill("user1@example.com");
+  await page.getByLabel("Email Address *").press("Tab");
+  await page.getByLabel("Password *").fill("password");
+  await page.getByLabel("Password *").press("Tab");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page.getByRole("button", { name: "Home" })).toBeInViewport();
+  await expect(
+    page.getByRole("button", { name: "Events" }).first()
+  ).toBeInViewport();
+  await expect(
+    page.getByRole("button", { name: "Forum" }).first()
+  ).toBeInViewport();
+});
+
+test("User enters incorrect inputs and fails to login", async ({ page }) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").fill("test");
+  await page.getByText("Your email is not valid");
+  await page.getByLabel("Email Address *").fill("test@test.com");
+  await expect(page.getByText("Your email is not valid")).not.toBeVisible();
+  await page.getByLabel("Password *").fill("1");
+  await page.getByText("You must have a password over").click();
+  await page.getByLabel("Password *").fill("Password");
+  await expect(
+    page.getByText("You must have a password over")
+  ).not.toBeVisible();
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByText("Your login credentials are incorrect").click();
+});
+
+test("User can login, enters the home page, then logs out and returns to the login page", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByLabel("Email Address *").click();
+  await page.getByLabel("Email Address *").fill("user1@example.com");
+  await page.getByLabel("Password *").click();
+  await page.getByLabel("Password *").fill("password");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page.getByText("Frank Castle")).toBeInViewport();
+  await page.getByRole("button", { name: "FC" }).click();
+  await page.getByText("Logout").click();
+  await expect(page.getByRole("button", { name: "Sign In" })).toBeInViewport();
+});
+
+const randomInt = Math.floor(Math.random() * 100000);
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByRole("tab", { name: "Register" }).click();
+  await page.getByLabel("Username *").click();
+  await page.getByLabel("Username *").fill(`user${randomInt}`);
+  await page.getByLabel("Username *").press("Tab");
+  await page.getByLabel("Email Address *").fill(`user${randomInt}@example.com`);
+  await page.getByLabel("Email Address *").press("Tab");
+  await page.getByLabel("Password *", { exact: true }).fill("password123");
+  await page.getByLabel("Password *", { exact: true }).press("Tab");
+  await page.getByLabel("Confirm Password *").fill("password123");
+  await page.getByRole("button", { name: "Sign Up" }).click();
+  await expect(page.getByText(`user${randomInt}`)).toBeInViewport();
+});
+
+test("User triggers incorrect input errors then corrects them", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:8080/login");
+  await page.getByRole("tab", { name: "Register" }).click();
+  await page.getByLabel("Username *").fill("a");
+  await page.getByLabel("Email Address *").fill("a");
+  await page.getByText("Your email is not valid");
+  await page.getByLabel("Email Address *").fill("abc@123.com");
+  await expect(page.getByText("Your email is not valid")).not.toBeVisible();
+  await page.getByLabel("Password *", { exact: true }).fill("passw");
+  await page.getByText("You must have a password over");
+  await page.getByLabel("Password *", { exact: true }).fill("password");
+  await expect(
+    page.getByText("You must have a password over")
+  ).not.toBeVisible();
+  await page.getByLabel("Confirm Password *").fill("a");
+  await page.getByText("Passwords must match");
+  await page.getByLabel("Confirm Password *").fill("password");
+  await expect(page.getByText("Passwords must match")).not.toBeVisible();
+});
